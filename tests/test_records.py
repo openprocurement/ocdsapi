74fc31577d4bfafe36eb5a7ca7e9dc85ff36868a
Yaroslav Shalenyk <yshalenyk@quintagroup.com>
2018-07-11 19:50:40 +0300

    Fixed tests

--- tests/test_records.py
+++ tests/test_records.py
@@ -4,17 +4,24 @@ from werkzeug.exceptions import NotFound
 
 
 def test_get(client, storage):
-    with client.get('/record.json?ocid=test_ocid') as response:
+    with client.get('/api/record.json?ocid=test_ocid') as response:
         assert response.json['releases'][0] == storage.get_ocid('test_ocid')
-    with client.get('/record.json?ocid=') as response:
+
+
+def test_get_not_found(client, storage):
+    with client.get('/api/record.json?ocid=') as response:
         assert response.status_code == 404
 
-def test_prepare_response(client, storage):
-    with client.get('/records.json?idsOnly=True') as response:
+
+def test_response_ids_only(client, storage):
+    with client.get('/api/records.json?idsOnly=True') as response:
         result = response.json
         assert result['records'] == [['spam_id', 'spam_ocid']]
-    with client.get('/records.json') as response:
+
+
+def test_prepare_response(client, storage):
+    with client.get('/api/records.json') as response:
         result = response.json
         assert result['records'] == [
-            'http://localhost/record.json?ocid=spam_ocid'
+            'http://localhost/api/record.json?ocid=spam_ocid'
         ]
