03810cdacb7db847b158c519cb2ebf524ee35fe8
Yaroslav Shalenyk <yshalenyk@quintagroup.com>
2018-11-13 14:20:15 +0200

    Added plugins support

--- ocdsapi/app.py
+++ ocdsapi/app.py
@@ -4,6 +4,7 @@ from pyramid.renderers import JSON
 from pyramid.authorization import ACLAuthorizationPolicy
 from pyramid.authentication import BasicAuthAuthenticationPolicy
 from pyramid.config import Configurator, ConfigurationError
+from zope.dottedname import resolve
 from ocdsmerge.merge import process_schema
 from ocdsapi.constants import SWAGGER, RECORD
 from ocdsapi.utils import format_release_package,\
@@ -24,7 +25,6 @@ def main(global_config, **settings):
         config.registry.settings['api_specs'] = swagger_data
         config.add_route('cornice_swagger.open_api_path', '/swagger.json')
         config.cornice_enable_openapi_explorer(api_explorer_path='/swagger.ui')
-        config.include('pyramid_celery')
         config.include('.models')
         config.add_renderer('simplejson', JSON(serializer=simplejson.dumps))
         config.add_request_method(format_release_package, name='release_package')
@@ -43,5 +43,12 @@ def main(global_config, **settings):
             config.registry.tokens = frozenset(tokens)
         config.set_authentication_policy(BasicAuthAuthenticationPolicy(check_credentials))
         config.registry.validator = fastjsonschema.compile(config.registry.schema)
+        apps = settings.get('apps', '').split(',')
+        for app in apps:
+            path, _, plugin = app.partition(':')
+            if not plugin:
+                plugin = 'includeme'
+            module = resolve.resolve(path)
+            getattr(module, plugin)(config)
         config.scan()
     return config.make_wsgi_app()
