28a6d119f258599df029addb4eed0464b6f2cf2f
Yaroslav Shalenyk <yshalenyk@quintagroup.com>
2018-11-13 20:45:37 +0200

    Added not found view

--- ocdsapi/views/forbidden.py
+++ ocdsapi/views/forbidden.py
@@ -1,6 +1,12 @@
-from pyramid.view import forbidden_view_config
+from pyramid.view import forbidden_view_config, notfound_view_config
 
 
 @forbidden_view_config(renderer='simplejson')
 def forbidden(request):
     return {'status': 'error', "description": "Forbidden"}
+
+
+@notfound_view_config(renderer='simplejson')
+def not_found(request):
+    request.response.status = 404
+    return {"status": "Not Found"}
